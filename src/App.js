8ef334c4afea145fe3676e6b85b22faeeda3c523
emontes <emontes@turista.com.mx>
2022-05-08 13:06:29 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import { ChatEngine } from 'react-chat-engine'
+
+import ChatFeed from './components/ChatFeed'
+import LoginForm from './components/LoginForm'
+import './App.css'
+
+const projectID = '454767c4-b75d-405e-b242-6e5f580f873a'
+
+const App = () => {
+  if (!localStorage.getItem('username')) return <LoginForm />
+
+  return (
+    <ChatEngine
+      height="100vh"
+      projectID={projectID}
+      userName={localStorage.getItem('username')}
+      userSecret={localStorage.getItem('password')}
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+      onNewMessage={() =>
+        new Audio(
+          'https://chat-engine-assets.s3.amazonaws.com/click.mp3',
+        ).play()
+      }
+    />
+  )
+}
+
+export default App
